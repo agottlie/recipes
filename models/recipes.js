c6682a8cbbcb7c00a52e31bb9d9a5b82c4dbf544
Andrew Gottlieb <andrewgottlieb@Andrews-MacBook-Air.local>
2020-07-21 16:14:31 -0400

    fixed link logic

--- models/recipes.js
+++ models/recipes.js
@@ -4,10 +4,11 @@ function create(recipe) {
 	let prefix = 'https://';
 	let prefix2 = 'http://';
 	let correct_link = recipe.link;
-	if ((correct_link.substr(0, prefix.length) != prefix) || (correct_link.substr(0,prefix2.length) != prefix2)) {
+	console.log(correct_link);
+	if (!((correct_link.substr(0, prefix.length) == prefix) || (correct_link.substr(0,prefix2.length) == prefix2))) {
 	    correct_link = prefix2 + correct_link;
 	}
-
+	console.log(correct_link);
 	return db.oneOrNone(`INSERT INTO recipes (name, link) VALUES ($1, $2) RETURNING *;`, [recipe.name, correct_link]);
 }
 
