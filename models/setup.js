179c90094323a2466db0c5b75fea85bc99c7fd2f
agottlie <agottlie@gmail.com>
2021-03-15 16:39:16 -0400

    ssl auth fix

--- models/setup.js
+++ models/setup.js
@@ -1,21 +1,21 @@
 pgp = require('pg-promise')();
 
-let pgdb = {
-    type: "postgres",
-    host: "ec2-52-204-232-46.compute-1.amazonaws.com",
-    port: 5432,
-    username: "wrdjznzsfqmdpp",
-    password:
-      "d7b7bb2784157fecbd1d2b9364e60d143a7861878db666e4e109a16d4c300a39",
-    database: "dcj3qa2r6sd8pl",
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      }
-    }
- };
 
-var db = pgp(pgdb);
+let ssl = null;
+if (process.env.NODE_ENV === 'development') {
+   ssl = {rejectUnauthorized: false};
+}
+
+const config = {
+   host: 'localhost',
+   port: 5432,
+   database: 'dcj3qa2r6sd8pl',
+   user: 'wrdjznzsfqmdpp',
+   password: 'd7b7bb2784157fecbd1d2b9364e60d143a7861878db666e4e109a16d4c300a39',
+   max: 30, // use up to 30 connections
+   ssl:ssl
+};
+
+const db = pgp(config);
 
 module.exports = db;
