deeb840c3a8c8470680cad41d5265ce54c681ee0
Andrew Gottlieb <andrewgottlieb@Mac.attlocal.net>
2024-12-13 14:10:58 -0800

    prep for render

--- models/setup.js
+++ models/setup.js
@@ -2,24 +2,24 @@ pgp = require('pg-promise')();
 
 
 //for localhost
-var db = pgp(process.env.DATABASE_URL || 'postgres://andrew.gottlieb@localhost:5432/recipes');
+// var db = pgp(process.env.DATABASE_URL || 'postgres://andrewgottlieb@localhost:5432/recipes');
 
 
 
-//for heroku
-// const config = {
-//    host: 'ec2-52-204-232-46.compute-1.amazonaws.com',
-//    port: 5432,
-//    database: 'dcj3qa2r6sd8pl',
-//    user: 'wrdjznzsfqmdpp',
-//    password: 'd7b7bb2784157fecbd1d2b9364e60d143a7861878db666e4e109a16d4c300a39',
-//    max: 30, // use up to 30 connections
-//    ssl: {
-//         rejectUnauthorized: false,
-//         require: true
-//    }
-// };
-// const db = pgp(config);
+// for render
+const config = {
+   host: 'dpg-cteb0i56l47c73b8ibtg-a',
+   port: 5432,
+   database: 'recipes_jqcm',
+   user: 'recipes_jqcm_user',
+   password: 'mHyLWXEGLGSN2gAOXNEzUaKibb1u2xy0',
+   max: 30, // use up to 30 connections
+   ssl: {
+        rejectUnauthorized: false,
+        require: true
+   }
+};
+const db = pgp(config);
 
 
 
