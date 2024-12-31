b560a78c481bd3837c9d737ca9da7719177a5d58
Andrew Gottlieb <agottlie@gmail.com>
2024-12-31 15:28:48 -0800

    changed db

--- models/setup.js
+++ models/setup.js
@@ -2,19 +2,19 @@ pgp = require('pg-promise')();
 
 
 //for localhost
-// var db = pgp(process.env.DATABASE_URL || 'postgres://andrewgottlieb@localhost:5432/recipes');
+var db = pgp(process.env.DATABASE_URL || 'postgres://andrewgottlieb@localhost:5432/recipes');
 
 
 
 // for render
-const config = {
-   host: 'dpg-cteb0i56l47c73b8ibtg-a',
-   port: 5432,
-   database: 'recipes_jqcm',
-   user: 'recipes_jqcm_user',
-   password: 'mHyLWXEGLGSN2gAOXNEzUaKibb1u2xy0'
-};
-const db = pgp(config);
+// const config = {
+//    host: 'dpg-cteb0i56l47c73b8ibtg-a',
+//    port: 5432,
+//    database: 'recipes_jqcm',
+//    user: 'recipes_jqcm_user',
+//    password: 'mHyLWXEGLGSN2gAOXNEzUaKibb1u2xy0'
+// };
+// const db = pgp(config);
 
 
 
